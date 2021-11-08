a018afd4cb9043ab6f655116e6f85362aa3186b9
V S <vatosarmat@yandex.ru>
2021-11-08 14:50:04 +1000

    instance setup script

--- ec2-ssh.bash
+++ ec2-ssh.bash
@@ -1,29 +1,63 @@
-function ec2ssh {
-  local -r user='ubuntu'
-  local ssh_cmd=('ssh' '-o' 'ConnectTimeout=3' '-o' 'ConnectionAttempts=1' '-X')
+_ec2_user='ubuntu'
+_ec2_dns_file="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.ec2-dns.txt"
+
+function __ec2_read_dns_from_file {
   #shellcheck disable=2155
-  local dns_file="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.ec2-dns"
-  local dns
-  if [[ -r "$dns_file" ]]; then
-    read -r dns < "$dns_file"
-  fi
+  local dns=
+  [[ -r "$_ec2_dns_file" ]] && read -r dns < "$_ec2_dns_file"
 
-  if [[ "$dns" ]] && "${ssh_cmd[@]}" "$user@$dns"; then
-    # Valid dns read from file
-    return
+  if [[ "$dns" ]]; then
+    echo "$dns"
+  else
+    return 1
   fi
+}
 
-  #no file, nd dns in it, or dns is bad
+function __ec2_query_dns {
+  local dns=
   dns="$(aws ec2 describe-instances \
     --query "Reservations[*].Instances[] | [?State.Name == 'running'] | [0].PublicDnsName" |
     cut -d'"' -f2)"
 
-  if [[ ! "$dns" ]] || [[ "$dns" = "null" ]]; then
+  if [[ "$dns" ]] && [[ "$dns" != "null" ]]; then
+    tee "$_ec2_dns_file" <<< "$dns"
+  else
     echo "No running instance to log in!" >&2
-    rm -f "$dns_file"
+    rm -f "$_ec2_dns_file"
+    return 1
+  fi
+}
+
+function ec2ssh {
+  local ssh_cmd=('ssh' '-o' 'ConnectTimeout=3' '-o' 'ConnectionAttempts=1' '-X')
+  local dns=
+
+  # If valid dns has been read from the file, return
+  dns="$(__ec2_read_dns_from_file)" && "${ssh_cmd[@]}" "$_ec2_user@$dns" ||
+    dns="$(__ec2_query_dns)" && "${ssh_cmd[@]}" "$_ec2_user@$dns"
+}
+
+function ec2cp {
+  local scp_cmd=('scp' '-o' 'ConnectTimeout=3' '-o' 'ConnectionAttempts=1')
+  local stuff="$1"
+
+  if [[ ! -r "$stuff" ]]; then
+    echo 'Argument - readable file or dir is required' >&2
     return 1
   fi
 
-  echo "$dns" > "$dns_file"
-  "${ssh_cmd[@]}" "$user@$dns"
+  if [[ -d "$stuff" ]]; then
+    scp_cmd+=('-r' "$stuff")
+  else
+    scp_cmd+=("$stuff")
+  fi
+
+  local dns=
+  if dns="$(__ec2_read_dns_from_file)"; then
+    "${scp_cmd[@]}" "$_ec2_user@$dns:/home/$_ec2_user/scp_inbox/$stuff"
+  else
+    dns="$(__ec2_query_dns)" &&
+      "${scp_cmd[@]}" "$_ec2_user@$dns:/home/$_ec2_user/scp_inbox/$stuff"
+  fi
 }
+complete -fd ec2cp
